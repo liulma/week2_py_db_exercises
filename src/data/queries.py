46d423a12907800ac29aec7de7c3dc7ea1a51970
liulma <linda.ulma@skillio.ai>
2025-01-21 09:46:26 +0200

    Python database exercises

---
+++ src/data/queries.py
@@ -0,0 +1,18 @@
+import psycopg2
+from config import config
+
+def connect():
+    con = None
+    try:
+        con = psycopg2.connect(**config())
+        cursor = con.cursot()
+        SQL = 'SELECT * FROM person;'
+        cursor.execute(SQL)
+        row = cursor.fetchone()
+        print(row)
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
